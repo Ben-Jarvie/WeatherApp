3fe3e7425ef738f1826a2ea5a4302d9d347ddb1c
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2022-11-07 11:55:49 -0600

    making final edits, cleaning up and refactoring

--- src/api.js
+++ src/api.js
@@ -2,8 +2,11 @@ export const geoApiOptions = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '403acd4b32mshb5c269a1ea859d8p126c3cjsn82c695e20bfd',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
 	}
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '3bf97abaaf27575bcce1ec5a37c80e5b';
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL, geoApiOptions } from '../../api';
+import { geoApiOptions, GEO_API_URL } from "../../api";
 
 const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
         return fetch(
-            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
-            .then(response => response.json())
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, 
+            geoApiOptions
+            )
+            .then((response) => response.json())
             .then((response) => {
                 return {
                     options: response.data.map((city) => {
@@ -16,16 +18,15 @@ const Search = ({ onSearchChange }) => {
                             value: `${city.latitude} ${city.longitude}`,
                             label: `${city.name}, ${city.countryCode}`,
                         };
-                    })
+                    }),
                 };
-            })
-            .catch(err => console.error(err));
-    }
+            });
+    };
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
-    }
+    };
 
     return (
         <AsyncPaginate
@@ -35,7 +36,7 @@ const Search = ({ onSearchChange }) => {
             onChange={handleOnChange}
             loadOptions={loadOptions}
         />
-    )
-}
+    );
+};
 
 export default Search;
